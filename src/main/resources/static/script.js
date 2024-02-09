e0a61aa0d7c418c38a55d65d14baab880aab001f
Bálint <balazs.balint5@gmail.com>
2024-02-09 22:46:36 +0100

    Added functions to switch between New Card button, and add card textfield

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,15 @@
 "use strict mode";
 
+const taskColumn = document.querySelector(".task-column");
+const addTaskBtns = document.querySelectorAll(".task-column__add-button");
+const inputContainers = document.querySelectorAll(
+  ".task-column-input-container"
+);
+
+let activeInputContainer;
+let hiddenAddButton;
+let activeTextarea;
+
 const testAPi = function () {
   fetch("http://localhost:8080/api/tasks")
     .then(function (response) {
@@ -10,4 +20,57 @@ const testAPi = function () {
     });
 };
 
-testAPi();
+//testAPi();
+
+const toggleButtonAndInputContainer = function (inputContainer, button) {
+  inputContainer.classList.toggle("hidden");
+  button.classList.toggle("hidden");
+};
+
+const switchActiveInputContainer = function (event) {
+  if (hiddenAddButton === undefined) {
+    hiddenAddButton = event.target;
+    activeInputContainer = hiddenAddButton.previousElementSibling;
+
+    toggleButtonAndInputContainer(hiddenAddButton, activeInputContainer);
+  } else if (event.target !== hiddenAddButton) {
+    toggleButtonAndInputContainer(hiddenAddButton, activeInputContainer);
+    hiddenAddButton = event.target;
+    activeInputContainer.firstElementChild.value = "";
+    activeInputContainer = hiddenAddButton.previousElementSibling;
+
+    toggleButtonAndInputContainer(hiddenAddButton, activeInputContainer);
+  }
+};
+
+const closeActiveInputContainer = function (event) {
+  if (
+    !activeInputContainer.contains(event.target) &&
+    hiddenAddButton !== undefined
+  ) {
+    toggleButtonAndInputContainer(activeInputContainer, hiddenAddButton);
+    activeInputContainer.firstElementChild.value = "";
+    activeInputContainer = undefined;
+    hiddenAddButton = undefined;
+  }
+};
+
+const init = function () {
+  addTaskBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      switchActiveInputContainer(e);
+    });
+  });
+};
+
+init();
+
+document.addEventListener("click", function (e) {
+  if (
+    hiddenAddButton !== undefined &&
+    e.target !== activeInputContainer &&
+    e.target !== hiddenAddButton
+  ) {
+    closeActiveInputContainer(e);
+  }
+});
