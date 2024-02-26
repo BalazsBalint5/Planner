2063917ff6f487b0d19e1ac044446444ddddd03c
Bálint <balazs.balint5@gmail.com>
2024-02-26 10:05:02 +0100

    Added topic service classes for basic crud methods

--- src/main/java/com/bb/planner/services/TopicService.java
+++ src/main/java/com/bb/planner/services/TopicService.java
@@ -1,2 +1,18 @@
-package com.bb.planner.services;public class TopicService {
+package com.bb.planner.services;
+
+import com.bb.planner.models.Task;
+import com.bb.planner.models.Topic;
+
+import java.util.List;
+
+public interface TopicService {
+
+    Topic getTopic(Topic topic);
+    Topic getTopicById(Integer topicId);
+    List<Topic> getAllTopic();
+    Topic addTopic(Topic topic);
+    Topic saveTaskForTopic(Integer topicId, Task task);
+    void deleteTopic(Topic topic);
+    void deleteTopicById(Integer topicId);
+    List<Task> getAllTaskFromTopicById(Integer topicId);
 }
--- src/main/java/com/bb/planner/services/TopicServiceImpl.java
+++ src/main/java/com/bb/planner/services/TopicServiceImpl.java
@@ -1,2 +1,61 @@
-package com.bb.planner.services;public class TopicServiceImpl {
+package com.bb.planner.services;
+
+import com.bb.planner.models.Task;
+import com.bb.planner.models.Topic;
+import com.bb.planner.repositories.hibernate.TopicRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TopicServiceImpl implements TopicService {
+
+    private final TopicRepository topicRepository;
+
+    public TopicServiceImpl(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    @Override
+    public Topic getTopic(Topic topic) {
+        return topicRepository.get(topic);
+    }
+
+    @Override
+    public Topic getTopicById(Integer topicId) {
+        return topicRepository.getById(topicId);
+    }
+
+    @Override
+    public List<Topic> getAllTopic() {
+        return topicRepository.getAll();
+    }
+
+    @Override
+    public Topic addTopic(Topic topic) {
+        return topicRepository.add(topic);
+    }
+
+    @Override
+    public Topic saveTaskForTopic(Integer topicId, Task task) {
+        Topic tempTopic = topicRepository.getById(topicId);
+        tempTopic.addTask(task);
+
+        return topicRepository.addTaskForTopic(tempTopic);
+    }
+
+    @Override
+    public void deleteTopic(Topic topic) {
+        topicRepository.delete(topic);
+    }
+
+    @Override
+    public void deleteTopicById(Integer topicId) {
+        topicRepository.deleteById(topicId);
+    }
+
+    @Override
+    public List<Task> getAllTaskFromTopicById(Integer topicId) {
+        return topicRepository.getTopicTasks(topicId);
+    }
 }
